aa803a28d4ba0ef9cd88b0a363ae0d8952e626b3
jimenavarro02 <jimenanavarroalvarez20@gmail.com>
2026-09-15 16:28:27 -0600

    Configura Spring Boot

---
+++ backend/src/main/java/cr/ac/ucr/paraiso/ie/c5l811/expresofast/ExpresoFastApplication.java
@@ -0,0 +1,11 @@
+package cr.ac.ucr.paraiso.ie.c5l811.expresofast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpresoFastApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ExpresoFastApplication.class, args);
+    }
+}
